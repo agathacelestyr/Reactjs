232dd3b7bdee038fb17b952624caf1b44173b9a7
agathacelestyr <agathacelesty123@gmail.com>
2022-09-01 18:15:42 +0530

    add

---
+++ Assessment2/store-app/src/api/authenticationdata/data.js
@@ -0,0 +1,22 @@
+export function getData(param) {
+  return [
+    {
+      id: 0,
+      username: "agathacelesty",
+      password: "12345",
+    },
+
+    {
+      agathacelesty: {
+        id: 0,
+        password: "12345c",
+      },
+    },
+    {
+      abishek: {
+        id: 0,
+        password: "12345a",
+      },
+    },
+  ];
+}
---
+++ Assessment2/store-app/src/screens/authentication/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style.css";
+import { Authenticate } from "../../components";
+
+export default function Authenication() {
+  return <Authenticate />;
+}
