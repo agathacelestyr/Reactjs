fdb960c2d570d8bbc3650062d5186671a72d85f4
agathacelestyr <agathacelesty123@gmail.com>
2022-09-06 09:48:29 +0530

    06/09/2022

--- Flask/first-api/restfulSetup.py
+++ Flask/first-api/restfulSetup.py
@@ -4,9 +4,5 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Store(Resource):
-    def get(self, name):
-        return{'store-name': name}
 
-
-api.add_resource
+app.run(debug=True, port=5000)
