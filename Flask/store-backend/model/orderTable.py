194dc21b4aee00a43200f2aaed32acd80cb64df0
agathacelestyr <agathacelesty123@gmail.com>
2022-09-15 14:56:05 +0530

    15/09/2022

--- Flask/store-backend/model/orderTable.py
+++ Flask/store-backend/model/orderTable.py
@@ -3,13 +3,26 @@ from db import db
 
 class OrdersTable(db.Model):
     __tablename__ = "sql_orders"
-    order_id = db.Column(db.Integer, primarykey=True, nullable=False)
+    order_id = db.Column(db.Integer, primarykey=True,
+                         nullable=False, backref="sql_orders")
     shipping_id = db.Column(db.Integer, nullable=False)
     products = db.Column(db.Text, nullable=False)
     customer_id = db.Column(db.Integer, nullable=False)
+    shippings = db.realtionship("sql_shipping")
 
     def __init__(self, shipping, products, custId) -> None:
         super().__init__()
         self.shipping_id = shipping
         self.products = products
         self.customer_id = custId
+
+    def add_order(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def json(self):
+        return {"id": self.order_id, "shippingId": self.shipping_id, "shippings": [ship.json() for ship in self.shippings.all()]}
+
+    @classmethod
+    def get_order_by_id(cls, order_id):
+        return cls.query.filter_by(order_id=order_id).first()
--- Flask/store-backend/model/shippingTable.py
+++ Flask/store-backend/model/shippingTable.py
@@ -9,9 +9,17 @@ class ShippingTable(db.Model):
     product_id = db.Column(db.Integer, nullable=False)
     location = db.Column(db.String(50), nullable=False)
     order = db.relationship("sql_orders")
+    status = db.Column(db.String(1),)
 
     def __init__(self, order_id, product_id, location) -> None:
         super().__init__()
         self.order_id = order_id
         self.product_id = product_id
         self.location = location
+
+    @classmethod
+    def get_shippings_by_order_id(cls, orderId):
+        cls.query.filter_by(order_id=orderId).all()
+
+    def json(self):
+        return {"id": self.ship_id, "orderId": self.order_id}
